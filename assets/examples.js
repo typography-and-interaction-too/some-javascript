14839a1584e8514991719cef7aa959619a6bc6bf
Michael Fehrenbach <mf@michaelfehrenbach.com>
2022-04-15 00:24:03 -0400

    Roughing it out.

---
+++ assets/examples.js
@@ -0,0 +1,11 @@
+console.log("Hello, world")
+
+// alert("These are annoying!")
+
+
+
+// This waits until the browser parses the page.
+window.addEventListener('DOMContentLoaded', () => {
+	console.log("This runs later.")
+});
+
